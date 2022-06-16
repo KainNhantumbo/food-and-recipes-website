472c2341e0e59940a7a963b2846568854fa22ea6
Kain Nhantumbo <nhantumbok@gmail.com>
2022-06-16 15:08:48 +0200

    feat: added new footer components structure.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,73 +1,79 @@
 import { FooterContainer } from '../styles/components/footer';
 import { BiSend } from 'react-icons/bi';
 import { useState } from 'react';
-import { FaArrowCircleRight } from 'react-icons/fa';
+import { FaArrowCircleRight, FaNewspaper } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<FooterContainer>
-			<section>
-				<h2>Loja</h2>
-				<ul>
-					<li>
-						<a href='/'>Início</a>
-					</li>
-					<li>
-						<a href='/about'>Sobre nós</a>
-					</li>
-					<li>
-						<a href='/contact'>Contacto</a>
-					</li>
-					<li>
-						<a href='/privacy-policy'>Política de Privacidade</a>
-					</li>
-					<li>
-						<a href='/terms-and-conditions'>Termos e Condições</a>
-					</li>
-				</ul>
-			</section>
-			<section>
-				<h2>Desenvolvedor</h2>
-				<ul>
-					<li>
-						<a href='/login'>Entrar</a>
-					</li>
-					<li>
-						<a href='/support'>Suporte</a>
-					</li>
-					<li>
-						<a href='/request-service'>Contratar serviços</a>
-					</li>
-				</ul>
-			</section>
 			<section className='newsletter'>
-				<h2>Newsletter</h2>
-				<p>
-					Subscreva-se à nossa newsletter <br></br>
-					enviando o seu e-mail, para <br></br>
-					saber quando um novo produto <br></br>
-					tiver sido lançado em seu e-mail.
-				</p>
-				<section>
-					<label htmlFor='email'>Email</label>
-					<div>
-						<input
-							type='email'
-							name='email'
-							placeholder='Digite o e-mail aqui...'
-							onChange={(e) => {}}
-						/>
-
-						<button>
-              <FaArrowCircleRight/>
-							<span>Enviar</span>
-						</button>
+				<div className='container'>
+					<div className='content'>
+						<h2>
+              <FaNewspaper/>
+              <span>Newsletter</span></h2>
+						<p>
+							Seja notificado com as últimas públicações em seu e-mail ao se inscrever em nossa newsletter.
+						</p>
 					</div>
-				</section>
+					<section className='form'>
+						<div>
+							<input
+								type='email'
+								name='email'
+								placeholder='Digite o e-mail aqui...'
+								onChange={(e) => {}}
+							/>
+							<button>
+								<span>Enviar</span>
+							</button>
+						</div>
+					</section>
+				</div>
 			</section>
-			<div className='copyright'>
-				<span>&copy; Copyright 2022 MealBlast, Moçambique.</span>
-			</div>
+			<section className='footer-components'>
+				<section>
+					<h2>Loja</h2>
+					<ul>
+						<li>
+							<a href='/'>Início</a>
+						</li>
+						<li>
+							<a href='/about'>Sobre nós</a>
+						</li>
+						<li>
+							<a href='/contact'>Contacto</a>
+						</li>
+						<li>
+							<a href='/privacy-policy'>Política de Privacidade</a>
+						</li>
+						<li>
+							<a href='/terms-and-conditions'>Termos e Condições</a>
+						</li>
+					</ul>
+				</section>
+				<section>
+					<h2>Desenvolvedor</h2>
+					<ul>
+						<li>
+							<a href='/login'>Entrar</a>
+						</li>
+						<li>
+							<a href='/support'>Suporte</a>
+						</li>
+						<li>
+							<a href='/request-service'>Contratar serviços</a>
+						</li>
+					</ul>
+				</section>
+
+				<div className='copyright'>
+					<span>
+						&copy; Copyright 2022 MealBlast and Umino Plus,{' '}
+						<address>Maputo - Moçambique.</address>{' '}
+					</span>
+				</div>
+			</section>
 		</FooterContainer>
 	);
 };
--- styles/components/footer.ts
+++ styles/components/footer.ts
@@ -3,59 +3,72 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
 	width: 100vw;
 	height: min-content;
-	display: grid;
-	grid-template-columns: repeat(2, 1fr) 1.5fr;
 	position: relative;
-
-	gap: 30px 20px;
-	padding: 20px;
-	background: rgb(${({ theme }) => theme.secondary});
-	color: rgb(${({ theme }) => theme.text});
-
-	@media screen and (max-width: 810px) {
-		grid-template-columns: repeat(2, 1fr);
-	}
-	@media screen and (max-width: 485px) {
-		grid-template-columns: 1fr;
-	}
-
-	::before {
-		content: '';
-		position: absolute;
-		top: -5px;
-		left: 0;
-		width: 100vw;
-		height: 5px;
-		background: rgba(${({ theme }) => theme.primary}, 0.8);
-	}
-
-	section {
-		h2 {
-			font-weight: 500;
-			font-size: 1.2rem;
-			line-height: 2.2rem;
-		}
-
-		li {
-			line-height: 1.6rem;
-			:hover {
-				color: rgb(${({ theme }) => theme.primary});
-			}
-		}
-	}
+	font-family: Inter, 'Open Sans', Montserrat, Poppins, 'PT Sans';
+	display: flex;
+	flex-direction: column;
+	gap: 50px;
 
 	.newsletter {
 		display: flex;
-		justify-content: flex-start;
+		justify-content: center;
 		flex-direction: column;
+		align-items: center;
 		gap: 12px;
 
-		p {
-			line-height: 1.2rem;
-		}
+		.container {
+			display: flex;
+			justify-content: space-between;
+			width: 100%;
+			max-width: 700px;
+			align-items: center;
+			gap: 10px;
+			background: rgb(${({ theme }) => theme.hover});
+			padding: 20px 50px;
+			border-radius: 10px;
 
-		label {
-			font-weight: 500;
+			.content {
+				display: flex;
+				flex-direction: column;
+				gap: 10px;
+
+				h2 {
+					font-size: 1.2rem;
+					font-weight: 500;
+				}
+				p {
+					line-height: 1.1rem;
+				}
+			}
+
+			.form {
+				div {
+					display: flex;
+					gap: 5px;
+					justify-content: flex-start;
+					flex-flow: row nowrap;
+
+					input {
+						border: 2px solid rgb(${({ theme }) => theme.primary});
+						border-radius: 5px;
+						padding: 5px;
+
+						@media screen and (max-width: 305px) {
+							width: 175px;
+						}
+
+						::placeholder {
+							font-size: 1rem;
+						}
+
+						:focus {
+							box-shadow: 0 0 10px rgb(${({ theme }) => theme.primary});
+							border: 2px solid rgb(${({ theme }) => theme.primary});
+							outline-color: rgb(${({ theme }) => theme.primary});
+						}
+					}
+				}
+			}
 		}
 
 		section {
@@ -68,52 +81,58 @@ export const FooterContainer = styled.footer`
 				font-size: 1rem;
 				border-radius: 5px;
 				cursor: pointer;
-				span, svg {
+				span,
+				svg {
 					pointer-events: none;
 				}
 				:hover {
 					box-shadow: 0 0 5px 1px rgb(${({ theme }) => theme.shadows});
 				}
 			}
-
-			div {
-				display: flex;
-				gap: 5px;
-				justify-content: flex-start;
-				flex-flow: row nowrap;
-
-				input {
-					border: 2px solid rgb(${({ theme }) => theme.primary});
-					border-radius: 5px;
-					padding: 5px;
-
-					@media screen and (max-width: 305px) {
-						width: 175px;
-					}
-
-					::placeholder {
-						font-size: 1rem;
-					}
-
-					:focus {
-						box-shadow: 0 0 10px rgb(${({ theme }) => theme.primary});
-						border: 2px solid rgb(${({ theme }) => theme.primary});
-						outline-color: rgb(${({ theme }) => theme.primary});
-					}
-				}
-			}
 		}
 	}
 
-	.copyright {
-		text-transform: uppercase;
-		font-weight: 500;
-		font-size: 0.8rem;
-		letter-spacing: 1px;
-		grid-column: 1/3;
+	.footer-components {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr) 1.5fr;
+		gap: 30px 20px;
+		padding: 20px;
+		background: rgb(${({ theme }) => theme.hover});
+		color: rgb(${({ theme }) => theme.font});
 
+		@media screen and (max-width: 810px) {
+			grid-template-columns: repeat(2, 1fr);
+		}
 		@media screen and (max-width: 485px) {
-			grid-column: 1/1;
+			grid-template-columns: 1fr;
+		}
+
+		section {
+			h2 {
+				font-weight: 500;
+				font-size: 1.2rem;
+				line-height: 2.2rem;
+			}
+
+			li {
+				line-height: 1.6rem;
+				:hover {
+					color: rgb(${({ theme }) => theme.primary});
+				}
+			}
+		}
+
+		.copyright {
+			text-transform: uppercase;
+			font-weight: 500;
+			font-size: 0.8rem;
+			letter-spacing: 1px;
+			grid-column: 1/3;
+			line-height: 1.2rem;
+
+			@media screen and (max-width: 485px) {
+				grid-column: 1/1;
+			}
 		}
 	}
 `;
