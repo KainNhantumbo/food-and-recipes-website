c11fb9ff55f08007ba674939c9a2a32a77ddd6b5
Kain Nhantumbo <nhantumbok@gmail.com>
2022-06-16 10:55:27 +0200

    feat: added privacy advisor component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,57 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import AppContext from '../context/AppContext'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import AppContext from '../context/AppContext';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { BiCheckCircle } from 'react-icons/bi';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AppContext>
-    <Component {...pageProps} />
-  </AppContext>
+	const [privacyAdvisor, setprivacyAdvisor] = useState();
+	// advisor management
+
+	// controls the state of privacy advisor message
+	const advisorController = () => {
+		setprivacyAdvisor(() => false);
+		localStorage.setItem('advisorState', JSON.stringify('false'));
+	};
+
+	useEffect(() => {
+		const advisorState = JSON.parse(localStorage.getItem('advisorState') || ``);
+		if (!advisorState) {
+			localStorage.setItem('advisorState', JSON.stringify('true'));
+			setprivacyAdvisor(() => true);
+		}
+
+		if (advisorState === 'true') {
+			setprivacyAdvisor(() => true);
+		} else {
+			setprivacyAdvisor(() => false);
+		}
+	}, []);
+  
+	return (
+		<AppContext>
+			<Component {...pageProps} />
+			{privacyAdvisor ? (
+				<section className='advisor'>
+					<div>
+						<span>
+							Ao usar a <strong>Hagira Brands</strong>, você concorda com os a
+							nossa{' '}
+							<a href='/privacy-policy'>
+								<strong>política de privacidade</strong>
+							</a>
+							.
+						</span>
+						<button onClick={advisorController}>
+							<BiCheckCircle />
+							<span>Aceitar</span>
+						</button>
+					</div>
+				</section>
+			) : null}
+		</AppContext>
+	);
 }
 
-export default MyApp
+export default MyApp;
