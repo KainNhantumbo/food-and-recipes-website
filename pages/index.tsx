cdece5476f9fcea2c2a20df13ce4ea77288a19f4
Kain Nhantumbo <nhantumbok@gmail.com>
2022-06-18 18:46:08 +0200

    dev: home page unuseless components and styles cleanup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import HeadPage from '../components/Head';
-import { BiCog } from 'react-icons/bi';
-import { FaArrowRight } from 'react-icons/fa';
 import { HomeContainer as Container } from '../styles/home';
 import Header from '../components/Header';
 import PageLayout from '../components/PageLayout';
@@ -14,30 +12,9 @@ const Home: NextPage = () => {
 	return (
 		<>
 			<HeadPage />
-			<Container>
-				<Header />
-				<PageLayout />
-				<main>
-					<article className='actions-container'>
-						<div className='action'>
-							<BiCog className='icon' />
-							<span>Gerenciar Recipes Blog</span>
-							<button onClick={() => router.push('/recipes-blog')}>
-								<FaArrowRight />
-								<span>Gerenciar</span>
-							</button>
-						</div>
-						<div className='action'>
-							<BiCog className='icon' />
-							<span>Gerenciar Usuários</span>
-							<button onClick={() => router.push('/users')}>
-								<FaArrowRight />
-								<span>Gerenciar</span>
-							</button>
-						</div>
-					</article>
-				</main>
-			</Container>
+			<Header />
+			<PageLayout />
+			<Container></Container>
 			<Footer />
 		</>
 	);
