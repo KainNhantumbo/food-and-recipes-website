d2cb1e871ae3fd343a7a871b6ced557512c1c6f1
Kain Nhantumbo <nhantumbok@gmail.com>
2022-06-19 17:48:20 +0200

    feat: added support for fetching post data at Post page

--- pages/post/[recipe_id].tsx
+++ pages/post/[recipe_id].tsx
@@ -1,23 +1,61 @@
-import { NextPage } from 'next';
+import { NextPage, NextPageContext } from 'next';
 import { useRouter } from 'next/router';
 import HeadPage from '../../components/Head';
 import { PostContainer as Container } from '../../styles/post';
 import Header from '../../components/Header';
 import PageLayout from '../../components/PageLayout';
 import Footer from '../../components/Footer';
+import { base_api_url } from '../../utils/utils';
+interface PostData {
+	cook_time: string;
+	hints: string;
+	title: string;
+	history: string;
+	image_alt: string;
+	image_url: string;
+	instructions: string;
+	serving_yield: string;
+	category: string;
+	description: string;
+	ingredients: string;
+	image: string;
+}
 
-const Post: NextPage = (): JSX.Element => {
+interface Props {
+	locale: [];
+	data: { post: PostData };
+}
+
+const Post: NextPage<Props> = ({ data }): JSX.Element => {
 	const router = useRouter();
+	const post = data.post;
+	console.log(post);
 
 	return (
 		<>
 			<HeadPage />
 			<Header />
 			<PageLayout />
-			<Container></Container>
+			<Container>
+				
+			</Container>
 			<Footer />
 		</>
 	);
 };
 
+export async function getServerSideProps(context: NextPageContext) {
+	try {
+		const response = await fetch(
+			`${base_api_url}/recipes/posts/${context.query.recipe_id}`
+		);
+		const data = await response.json();
+
+		return {
+			props: { locale: context.query, data },
+		};
+	} catch (err) {
+		console.log(err);
+	}
+}
 export default Post;
